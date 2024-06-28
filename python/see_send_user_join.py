0e34e01869688cd8768909718821d8e309500bb7
fabrice1618 <fabrice1618@gmail.com>
2024-06-28 11:26:35 +0000

    TP SEE 20240628

---
+++ python/see_send_user_join.py
@@ -0,0 +1,32 @@
+import paho.mqtt.client as mqtt
+import json
+from datetime import datetime
+import time
+
+# Configuration du broker MQTT
+broker = "mosquitto"  # Remplacez par l'adresse de votre broker MQTT
+port = 1883
+topic = "user/join"
+
+# Fonction de rappel (callback) lorsque le client se connecte au broker
+def on_connect(client, userdata, flags, rc):
+    print(f"Connected with result code {rc}")
+
+# Création d'une instance de client MQTT
+client = mqtt.Client()
+
+# Assignation des fonctions de rappel
+client.on_connect = on_connect
+
+# Connexion au broker
+client.connect(broker, port, 60)
+
+message = {
+    "name": "nom utilisateur",
+}
+message_json = json.dumps(message)
+print(f"Publishing message: {message_json}")
+client.publish(topic, message_json)
+
+print("Disconnecting from broker")
+client.disconnect()
